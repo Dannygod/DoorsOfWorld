072246a73c27114ca12b96ff79f44fe53ccfd5e5
Danny Hsu <danny539425@gmail.com>
2025-04-28 18:17:56 +0800

    0428

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ document.querySelector('.modal-container').addEventListener('click', (e) => {
 });
 
 const carousel = document.querySelector('#carousel');
-const images = carousel.querySelectorAll('img');
-const prevBtn = carousel.querySelector('.prev');
-const nextBtn = carousel.querySelector('.next');
+const images = carousel.querySelectorAll('#i');
+const images2 = carousel.querySelectorAll('#j');
+const prevBtn = carousel.querySelector('#prev');
+const nextBtn = carousel.querySelector('#next');
+const prevBtn2 = carousel.querySelector('#prev2');
+const nextBtn2 = carousel.querySelector('#next2');
 let currentIndex = 0;
-const intervalTime = 5000; // 5秒切換一次
+let currentIndex2 = 0;
+const intervalTime = 3000; // 5秒切換一次
 
 // 顯示指定索引的圖片
 function showImage(index) {
@@ -26,21 +30,33 @@ function showImage(index) {
     });
     currentIndex = index;
 }
-
-// 下一張
 function nextImage() {
     const nextIndex = (currentIndex + 1) % images.length;
     showImage(nextIndex);
 }
-
-// 上一張
 function prevImage() {
     const prevIndex = (currentIndex - 1 + images.length) % images.length;
     showImage(prevIndex);
 }
 
+function showImage2(index) {
+    images2.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+    });
+    currentIndex2 = index;
+}
+function nextImage2() {
+    const nextIndex = (currentIndex2 + 1) % images2.length;
+    showImage2(nextIndex);
+}
+function prevImage2() {
+    const prevIndex = (currentIndex2 - 1 + images2.length) % images2.length;
+    showImage2(prevIndex);
+}
+
 // 自動播放
 let autoPlay = setInterval(nextImage, intervalTime);
+let autoPlay2 = setInterval(nextImage2, intervalTime);
 
 // 點擊按鈕
 nextBtn.addEventListener('click', () => {
@@ -48,15 +64,27 @@ nextBtn.addEventListener('click', () => {
     nextImage();
     autoPlay = setInterval(nextImage, intervalTime); // 重啟自動播放
 });
-
 prevBtn.addEventListener('click', () => {
     clearInterval(autoPlay);
     prevImage();
     autoPlay = setInterval(nextImage, intervalTime);
 });
 
-// 滑鼠懸停暫停自動播放
-carousel.addEventListener('mouseenter', () => clearInterval(autoPlay));
-carousel.addEventListener('mouseleave', () => {
-    autoPlay = setInterval(nextImage, intervalTime);
+nextBtn2.addEventListener('click', () => {
+    clearInterval(autoPlay2); // 停止自動播放
+    nextImage2();
+    autoPlay2 = setInterval(nextImage, intervalTime); // 重啟自動播放
 });
+prevBtn2.addEventListener('click', () => {
+    clearInterval(autoPlay2);
+    prevImage2();
+    autoPlay2 = setInterval(nextImage, intervalTime);
+});
+
+// 滑鼠懸停暫停自動播放
+// carousel.addEventListener('mouseenter', () => clearInterval(autoPlay));
+// carousel.addEventListener('mouseleave', () => {
+//     autoPlay = setInterval(nextImage, intervalTime);
+// });
+
+
